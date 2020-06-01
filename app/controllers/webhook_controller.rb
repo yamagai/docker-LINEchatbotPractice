5b2c2794320d61d40ca24e82d2e95a466ffd9e27
yamagai <kohei.yamamoto919@gmail.com>
2020-06-01 11:41:33 +0900

    env導入、オウム返し実装

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,26 @@
+class WebhookController < ApplicationController
+
+  def callback
+    body = request.body.read
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+    "OK"
+  end
+end
