3693d93dfb405e0fbc76e44e3429c2ceb0475f3f
yamagai <kohei.yamamoto919@gmail.com>
2020-06-02 20:38:57 +0900

    LINEで送信されてきた画像を保存する機能実装途中

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,24 +3,21 @@ class WebhookController < ApplicationController
   def callback
     body = request.body.read
     events = client.parse_events_from(body)
+    messageId = events[0]["message"]["id"]
 
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
+    client = Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
-    "OK"
+    response = client.get_message_content(messageId)
+
+    case response
+    when Net::HTTPSuccess then
+      tf = Tempfile.open("content")
+      tf.write(response.body)
+    else
+      p "#{response.code} #{response.body}"
+    end
+
   end
 end
